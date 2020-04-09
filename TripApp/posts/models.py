65416ea804d2a412ce4c17a0ee2cb367df207922
aleksandergalganski <olekgalgan@gmail.com>
2020-04-10 00:55:28 +0200

    Add post model
    Add post model and tests for posts model

--- TripApp/posts/models.py
+++ TripApp/posts/models.py
@@ -1,2 +1,57 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.conf import settings
 
+from taggit.managers import TaggableManager
+
+from PIL import Image
+
+
+class ActivePostsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActivePostsManager, self).get_queryset().filter(active=True)
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             related_name='posts')
+    about = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='post_images/%Y/%m/%d/', null=True, blank=True)
+    tags = TaggableManager()
+    location = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                   related_name='posts_likes')
+    # Default Manager
+    objects = models.Manager()
+    # Custom Manager for active posts
+    actives = ActivePostsManager()
+
+    def __str__(self):
+        return f'{self.name}-{self.user.username}-{self.created}'
+
+    def get_like_url(self):
+        return reverse('posts:post_like', args=[self.pk])
+
+    def get_absolute_url(self):
+        return reverse('posts:post_detail', args=[self.id])
+
+    @property
+    def get_likes_count(self):
+        return self.likes.count()
+
+    def save(self, *args, **kwargs):
+        """ Resizing an image"""
+        super(Post, self).save(*args, **kwargs)
+
+        if self.image:
+            img = Image.open(self.image.path)
+
+            if img.height > 500 or img.width > 500:
+                output_size = (500, 500)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
---
+++ TripApp/posts/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+from ..models import Post
+
+class ActivePostsManagerTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(name='test_post', user=self.user, about='...',
+                                        location='Kolobrzeg', active=False)
+        Post.objects.create(name='test_post', user=self.user, about='...',
+                                        location='Kolobrzeg', active=True)
+        Post.objects.create(name='test_post', user=self.user, about='...',
+                                        location='Kolobrzeg', active=True)
+
+    def should_return_two_posts(self):
+        posts = Post.actives()
+        self.assertEquals(1, len(posts))
+
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test',
+                                             password='testpassword')
+        self.user.save()
+        self.post = Post.objects.create(name='test_post', user=self.user, about='...',
+                                        location='Kolobrzeg')
+        self.
+    def test_str_method(self):
+        self.assertEquals(str(self.post), f'test_post-test-{self.post.created}')
+
+    def test_get_absolute_url_method(self):
+        self.assertEquals(self.post.get_absolute_url(), f'/posts/{self.post.id}/')
+
+    def test_get_likes_count(self):
+        pass
+        # todo
