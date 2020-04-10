965293896b3132939e9751ef43759c10a808097d
aleksandergalganski <olekgalgan@gmail.com>
2020-04-10 14:32:37 +0200

    Add Profile Model
    Add profile model and tests for profile model

--- TripApp/users/models.py
+++ TripApp/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, null=True)
+    bio = models.TextField(max_length=500)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
+
+    def get_absolute_url(self):
+        return reverse('users:user_detail', args=[self.user.username])
+
+    def save(self, *args, **kwargs):
+        """ Resizing an image """
+        super(Profile, self).save(*args, **kwargs)
+
+        if self.image:
+            img = Image.open(self.image.path)
+
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
+
---
+++ TripApp/users/tests/test_models.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from ..models import Profile
+
+
+class ProfileModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test',
+                                             email='test@email.com',
+                                             password='valid_password')
+        self.profile = Profile.objects.create(user=self.user)
+        self.profile.save()
+
+    def test_str_method(self):
+        self.assertEquals(str(self.profile), 'Profile for user test')
+
+    def test_get_absolute_url_method(self):
+        self.assertEquals(self.profile.get_absolute_url(), '/account/users/test')
