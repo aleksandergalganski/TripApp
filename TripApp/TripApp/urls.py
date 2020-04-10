a1105ad852f3eae198ef953465f647d38b904128
aleksandergalganski <olekgalgan@gmail.com>
2020-04-10 14:53:05 +0200

    Add registration templates

--- TripApp/TripApp/urls.py
+++ TripApp/TripApp/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
+    path('users/', include('users.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- TripApp/users/views.py
+++ TripApp/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import User
+from django.contrib import messages
 
-# Create your views here.
+from .models import Profile
+from .forms import UserRegistrationForm, UserEditForm, ProfileEditForm
+
+
+def register(request):
+    if request.method == 'POST':
+        register_form = UserRegistrationForm(request.POST)
+        if register_form.is_valid():
+            new_user = register_form.save(commit=False)
+            new_user.set_password(register_form.cleaned_data['password'])
+            new_user.save()
+            # Create new profile associated with new user
+            Profile.objects.create(user=new_user)
+            messages.success(request, 'Your account has been created')
+            return render(request, 'account/register_done.html')
+    else:
+        register_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'register_form': register_form})
